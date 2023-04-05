f1d3678dc9367fe98391f463363719251be696c9
LUNARWOLF94 <61485783+dezmonnpaul@users.noreply.github.com>
2023-04-05 11:16:31 +0100

    Adding Debounce and Struct
    Created a Debounce function and a struct for Task 2 and 3. Also added infinte loops for tasks 1-5

--- Tasks/Tasks.ino
+++ Tasks/Tasks.ino
@@ -4,7 +4,6 @@
 #define Task2MinFreq (333)                          //Defining the mimimum frequency for Task 2
 #define Task2MaxFreq (1000)                         //Defining the maximum frequency for Task 2 (Frequency range increased by 6 (-3, and +3) to allow a 3Hz error during detection)
 
-unsigned long Freq02;                                 //Defining a variable to store the frequency measures in Task 2
 bool Signal2;                                         //Defining a variable to store the initial signal when Task 2 begins
 unsigned long time02;                                 //Defining a variable to store the half period measured in Task 2 
 int Task2MaxPeriod=1000000/((Task2MinFreq));          //Defining a variable to store the maximum period expected in Task 2 
@@ -14,7 +13,6 @@ int Task2MinPeriod=1000000/((Task2MaxFreq));          //Defining a variable to s
 #define Task3MinFreq (500)                          //Defining the mimimum frequency for Task 3
 #define Task3MaxFreq (1000)                         //Defining the maximum frequency for Task 3 (Frequency range increased by 6 (-3, and +3) to allow a 3Hz error during detection)
 
-unsigned long Freq03;                                 //Defining a variable to store the frequency measures in Task 3
 bool Signal3;                                         //Defining a variable to store the initial signal when Task 3 begins
 unsigned long time03;                                 //Defining a variable to store the half period measured in Task 3 
 int Task3MaxPeriod=1000000/((Task3MinFreq));          //Defining a variable to store the maximum period expected in Task 3 
@@ -33,74 +31,123 @@ int average04[]={0,0};                                //Defining an array to sto
 int Freq02_Percentage;                                //Defining a variable to store the frequency measured in Task 2 betweeen the range 0 to 99.
 int Freq03_Percentage;                              //Defining a variable to store the frequency measured in Task 3 betweeen the range 0 to 99.
 
-
-void Task1(){                               //Outputs a  digital signal that is 
-  digitalWrite(Out1,HIGH);                           //HIGH for 200us,   
-  delayMicroseconds(200);
-  digitalWrite(Out1,LOW);                            //LOW for 50us,
-  delayMicroseconds(50);
-  digitalWrite(Out1,HIGH);                           //and HIGH for 30us,
-  delayMicroseconds(30);
-  digitalWrite(Out1,LOW);                   //the signal is then kept to be LOW.
-}
-
-void Task2(){                                   
-  Signal2=digitalRead(In2);                                     //Stores the initial state of the received signal
-  time02=0;                                                     //Initialise time02 to 0
-  time02=pulseIn(In2,!Signal2,Task2MaxPeriod);                  //waits for the signal to change state and determines the time it takes before returning to the original state (half period of the input signal).
-                                                                //if the signal takes more than Task2MaxPeriod to change, the code stops waiting and returns 0
-  if ((time02<(Task2MinPeriod/2)) && time02>0){                 //Determines if the measured half period is too long, returns 500 (half period of max frequency) if it is.
-    time02=(Task2MinPeriod/2);
-  }
-  else if (time02<=0||time02>(Task2MaxPeriod/2)){                             //Determines if the measured half period has timed-out(pulseIn returns a 0) or is longer than the expected maximum value, returns 1500 (half period of min frequency) if it is.
-    time02=(Task2MaxPeriod/2);
-  }
-  Freq02=1000000/(2*time02);                                    //Calculates the frequency of the input signal based on the measured half period.
+struct Frequencies {
+  unsigned long Freq02;                                 //Defining a variable to store the frequency measures in Task 2
+  unsigned long Freq03;                                 //Defining a variable to store the frequency measures in Task 3
 };
 
-void Task3(){                                  
-  Signal3=digitalRead(In3);                                     //Stores the initial state of the received signal
-  time03=0;                                                     //Initialise time03 to 0
-  time03=pulseIn(In3,!Signal3,Task3MaxPeriod);                  //waits for the signal to change state and determines the time it takes before returning to the original state (half period of the input signal).
+struct Frequencies Freq23;
+
+#define In_DEBOUNCE 9
+
+void Task1(){  
+  while(1){
+    digitalWrite(Out1,HIGH);                           //HIGH for 200us,   
+    delayMicroseconds(200);
+    digitalWrite(Out1,LOW);                            //LOW for 50us,
+    delayMicroseconds(50);
+    digitalWrite(Out1,HIGH);                           //and HIGH for 30us,
+    delayMicroseconds(30);
+    digitalWrite(Out1,LOW);
+                       //the signal is then kept to be LOW.
+  }                             //Outputs a  digital signal that is 
+}
+
+void Task2(){      
+  while(1){
+    Signal2=digitalRead(In2);                                     //Stores the initial state of the received signal
+    time02=0;                                                     //Initialise time02 to 0
+    time02=pulseIn(In2,!Signal2,Task2MaxPeriod);                  //waits for the signal to change state and determines the time it takes before returning to the original state (half period of the input signal).
+                                                                //if the signal takes more than Task2MaxPeriod to change, the code stops waiting and returns 0
+    if ((time02<(Task2MinPeriod/2)) && time02>0){                 //Determines if the measured half period is too long, returns 500 (half period of max frequency) if it is.
+      time02=(Task2MinPeriod/2);
+    }
+    else if (time02<=0||time02>(Task2MaxPeriod/2)){                             //Determines if the measured half period has timed-out(pulseIn returns a 0) or is longer than the expected maximum value, returns 1500 (half period of min frequency) if it is.
+      time02=(Task2MaxPeriod/2);
+    }
+    Freq23.Freq02=1000000/(2*time02);                                    //Calculates the frequency of the input signal based on the measured half period.
+  }                             
+  
+};
+
+void Task3(){
+  while(1){
+    Signal3=digitalRead(In3);                                     //Stores the initial state of the received signal
+    time03=0;                                                     //Initialise time03 to 0
+    time03=pulseIn(In3,!Signal3,Task3MaxPeriod);                  //waits for the signal to change state and determines the time it takes before returning to the original state (half period of the input signal).
                                                                 //if the signal takes more than Task2MaxPeriod to change, the code stops waiting and returns 0  
-  if ((time03<(Task3MinPeriod/2)) && time03>0){                 //Determines if the measured half period is too long, returns 500 (half period of max frequency) if its is.
-    time03=(Task3MinPeriod/2);
-  }
-  else if (time03<=0||time03>((Task3MaxPeriod/2))){                             //Determines if the measured half period has timed-out(pulseIn returns a 0) or is longer than the expected maximum value, returns 1000 (half period of min frequency) if it is.
-    time03=(Task3MaxPeriod/2);
-  }
-  Freq03=1000000/(2*time03);                                    //Calculates the frequency of the input signal based on the measured half period.
+    if ((time03<(Task3MinPeriod/2)) && time03>0){                 //Determines if the measured half period is too long, returns 500 (half period of max frequency) if its is.
+      time03=(Task3MinPeriod/2);
+    }
+    else if (time03<=0||time03>((Task3MaxPeriod/2))){                             //Determines if the measured half period has timed-out(pulseIn returns a 0) or is longer than the expected maximum value, returns 1000 (half period of min frequency) if it is.
+      time03=(Task3MaxPeriod/2);
+    }
+    Freq23.Freq03=1000000/(2*time03);
+  }                                  
+                                      //Calculates the frequency of the input signal based on the measured half period.
 };
 
 void Task4(){
-  average04[0]=0;                               //Resets the average value to 0
-  average04[1]=0;                               //Resets the number of readings to 0
-  Value04[index04++]=analogRead(In4);           //Reads the analogue value of the Input and stores it in Value04 at a position based on index04, index 04 is then incremented to allow the next reading to be stored in the neighbouring position
-  if(index04==4){                               // Resets index04 to 0 if it becomes to large
-    index04=0;
-  }
-  for(int i=0; i<4;i++){                        //adds all the values stored in Value04 together
-    if(Value04[i]>0){                           //checks if the value is not 0
-      average04[0]+=Value04[i];
-      if(average04[1]<4){                       // increments the number of readings if the value checked is not 0
-        average04[1]++;
+  while(1) {
+    average04[0]=0;                               //Resets the average value to 0
+    average04[1]=0;                               //Resets the number of readings to 0
+    Value04[index04++]=analogRead(In4);           //Reads the analogue value of the Input and stores it in Value04 at a position based on index04, index 04 is then incremented to allow the next reading to be stored in the neighbouring position
+    if(index04==4){                               // Resets index04 to 0 if it becomes to large
+      index04=0;
+    }
+    for(int i=0; i<4;i++){                        //adds all the values stored in Value04 together
+      if(Value04[i]>0){                           //checks if the value is not 0
+        average04[0]+=Value04[i];
+        if(average04[1]<4){                       // increments the number of readings if the value checked is not 0
+          average04[1]++;
+        }
       }
     }
+    average04[0]/=average04[1];                   // Computes the average of the reading staken so far
+    if(average04[0]>(MAX_Range/2)){                    //Determines if : average > half of the maximum value
+      digitalWrite(Out4,HIGH);                    //Turns ON a LED if average > half of the maximum value
+    }
+    else{digitalWrite(Out4,LOW);}                 //Turns OFF the LED otherwise
+    taskYIELD();
   }
-  average04[0]/=average04[1];                   // Computes the average of the reading staken so far
-  if(average04[0]>(MAX_Range/2)){                    //Determines if : average > half of the maximum value
-    digitalWrite(Out4,HIGH);                    //Turns ON a LED if average > half of the maximum value
-   }
-  else{digitalWrite(Out4,LOW);}                 //Turns OFF the LED otherwise
+  
+  
 };
 
 void Task5(){
-  Freq02_Percentage=99*(Freq02-Task2MinFreq)/(Task2MaxFreq-Task2MinFreq);      //Converts the value of the frequency measured in Task 2 to a value between 0 and 99
-  Freq03_Percentage=99*(Freq03-Task3MinFreq)/(Task3MaxFreq-Task3MinFreq);      //Converts the value of the frequency measured in Task 3 to a value between 0 and 99
-  char string_Output[5];                                                        //Creates a stringOutput based on the integer values of Freq02_Percentage and Freq03_Percentage
-  sprintf(string_Output,"%d,%d",Freq02_Percentage,Freq03_Percentage);
-  Serial.println(string_Output);                                                //Outputs the string to the serial monitor
+  while(1){
+    Freq02_Percentage=99*(Freq23.Freq02-Task2MinFreq)/(Task2MaxFreq-Task2MinFreq);      //Converts the value of the frequency measured in Task 2 to a value between 0 and 99
+    Freq03_Percentage=99*(Freq23.Freq03-Task3MinFreq)/(Task3MaxFreq-Task3MinFreq);      //Converts the value of the frequency measured in Task 3 to a value between 0 and 99
+    char string_Output[5];                                                        //Creates a stringOutput based on the integer values of Freq02_Percentage and Freq03_Percentage
+    sprintf(string_Output,"%d,%d",Freq02_Percentage,Freq03_Percentage);
+    Serial.println(string_Output);
+  }                                            //Outputs the string to the serial monitor
 };
+
+void Debounce(){
+  bool currentRead=0;
+  bool previousBounces=0;
+  uint32_t previousReading=0xFFFFFFFF;
+  uint32_t maxReading=0x7FFFFFFF;
+  bool callLED;
+
+  while(1){
+    currentRead=!!digitalRead(In_DEBOUNCE);
+    previousBounces=(previousBounces<<1)|currentRead;
+    if ((previousBounces & maxReading)==maxReading||(previousBounces & maxReading)==0){
+      if(currentRead!=previousReading){
+        callLED= !!currentRead;
+        
+      }
+    }
+    taskYIELD();
+  }
+
+}
+
+void LEDControl(){
+
+}
 
 void setup() {
   Serial.begin(9600);          //Stats the serial communication at a baud rate of 9600 bps
